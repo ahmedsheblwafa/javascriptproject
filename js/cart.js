171d40453bfc72832ee607f38b6298c4747e6793
ahmed_shebl_wafa <ahmedsheblwafa@gmail.com>
2020-12-01 01:00:14 -0800

    making cart dynamic

--- js/cart.js
+++ js/cart.js
@@ -1,22 +1,41 @@
 if (JSON.parse(localStorage.getItem("cartArr")))
     {
+        var totalCost = function() {
+                    var sum = 0;
+                for (var i=0 ; i<cartArr.length ; i++)
+                {sum += Number(table.rows[i+2].cells[5].innerText);
+                var cost = document.getElementById("totalCost")
+                         cost.innerHTML = sum;
+                }
+            
+            }
+        var delet = function(e){
+            var cartArr = JSON.parse(localStorage.getItem("cartArr"))
+            e.preventDefault()
+            var row = e.target.parentNode.parentNode
+            cartArr.splice(row.rowIndex-2,1)
+            localStorage.setItem('cartArr', JSON.stringify(cartArr))
+            table.removeChild(row)
+          }
         var dec = function(e){
             e.preventDefault()
             var row = e.target.parentNode.parentNode
             if(Number(row.cells[2].innerText)>1)
             {   
                 
-                console.log(row);
+                
                 row.cells[2].innerHTML=Number(row.cells[2].innerText)-1
-                cell6.innerHTML = cell3.innerHTML*cell5.innerHTML
+                row.cells[5].innerHTML = row.cells[2].innerHTML*row.cells[4].innerHTML
+                totalCost()
             }
         }
             var inc = function(e){
                 e.preventDefault()
                     var row = e.target.parentNode.parentNode
-                    console.log(row);
+                   
                     row.cells[2].innerHTML=Number(row.cells[2].innerText)+1
-                    cell6.innerHTML = cell3.innerHTML*cell5.innerHTML
+                    row.cells[5].innerHTML = row.cells[2].innerHTML*row.cells[4].innerHTML
+                    totalCost()
                 }
         var cartArr = JSON.parse(localStorage.getItem("cartArr"))
         for(var i=0 ; i<cartArr.length ; i++)
@@ -43,6 +62,8 @@ if (JSON.parse(localStorage.getItem("cartArr")))
           
            cell2.addEventListener("click",dec)
            cell4.addEventListener("click",inc)
+           cell7.addEventListener("click",delet)
+           
             // appending childs ---------------------------------------------------------------------------------------------------
             table.appendChild(row)
             row.appendChild(cell1)
@@ -56,6 +77,6 @@ if (JSON.parse(localStorage.getItem("cartArr")))
             
 
           }
-
-
+          
+            totalCost();
     }
