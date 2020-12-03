a96e5f9d96c6073c83af223e70f32e1d172e320d
ahmed_shebl_wafa <ahmedsheblwafa@gmail.com>
2020-12-03 05:08:19 -0800

    login warning issue resolved

--- js/login.js
+++ js/login.js
@@ -2,19 +2,23 @@ var mail = document.getElementById("email");
 var password2 = document.getElementById("password2");
 var table = document.getElementById("content");
 var warning = document.getElementById("warning");
+var valid =1 ;
 var check = function()
 {if (JSON.parse(localStorage.getItem("userData")))
     {
+        
         var userArray = JSON.parse(localStorage.getItem("userData"));
         for (var i=0 ; i<userArray.length ; i++)
         {
             if(userArray[i].email==mail.value & userArray[i].password==password2.value)
-            window.location.href = "../html/index.html";
-            else{
-                warning.style.display="inline";
-                
-            }
+            {window.location.href = "../html/index.html";
+            valid =0 ;}
+            
+            
         }
+        if(valid==1)
+        {warning.style.display="inline";}
+        valid =1 ;
     }
 else {
     warning.style.display="inline";
